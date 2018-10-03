3b0bf9a877dced300ad03ec787f1b3b8b784a53b
StonePunch <antonio.lopes1997@gmail.com>
2018-10-03 22:56:42 +0100

    Added some interaction, started integrating the facial recognition api but it seems to have some problems on their end, will try again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 // Components
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
+const app = new Clarifai.App({
+  apiKey: 'Your_API_Key'
+});
 
 const particlesOptions = {
   particles: {
@@ -21,6 +26,29 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        // do stuff
+      },
+      function(err) {
+        // error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,7 +58,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onButtonSubmit} />
         {/* <FaceRecognition /> */}
       </div>
     );
